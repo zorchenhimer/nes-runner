b9eaa4b2f1e7110431c7df3213d6d641ba6f0a19
Zorchenhimer <zorchenhimer@gmail.com>
2018-11-17 15:09:05 -0500

    Draw "Press Start" in the center of Ded screen
    Draw "Press Start" and center it on screen with a sprite zero hit and
    made it fade from grey to white and back again.

--- ded.asm
+++ ded.asm
@@ -78,7 +78,7 @@ Ded_Frame:
 
     lda #$21
     sta $2006
-    lda #$8C
+    lda #$8B
     sta $2006
 
     ldx #0
@@ -90,31 +90,139 @@ Ded_Frame:
     jmp @textLoop
 
 @txtEnd:
+
+    lda #$22
+    sta $2006
+    lda #$CB
+    sta $2006
+
+    ldx #0
+@startLoop:
+    lda DedStart, x
+    beq @startEnd
+    sta $2007
+    inx
+    jmp @startLoop
+
+@startEnd:
+    ; attributes for "Press Start"
+    lda #$23
+    sta $2006
+    lda #$EA
+    sta $2006
+
+    lda #$55
+    sta $2007
+    sta $2007
+    sta $2007
+    sta $2007
+
     ; reset the frame pointer to avoid fading again
     lda #<GameOverWait
     sta DoFramePointer
     lda #>GameOverWait
     sta DoFramePointer+1
 
-    lda #0
+    lda DedStartPal
+    sta DED_START_PAL
 
+    lda #0
+    sta TmpX    ; current color index
+    lda #2
+    sta TmpY    ; countdown to next switch
+
+    ; Sprite zero stuff
+    lda #$03
+    sta SPZ_IDX
+    lda #0
+    sta SPZ_ATT
+    sta SPZ_X
+    lda #127
+    sta SPZ_Y
+
+    ; non-transparent "black" for sprite zero
+    lda #$1D
+    sta DED_SPZ_PAL
+
+    lda #$1D
+    sta DED_SPZ_BG_PAL
+
+    ; BG tile for sprite zero
+    lda #$22
+    sta $2006
+    lda #$00
+    sta $2006
+
+    lda #$01
+    sta $2007
+
+    ; Attribute byte for BG tile for sprite zero
+    lda #$23
+    sta $2006
+    lda #$E0
+    sta $2006
+
+    lda #$AA
+    sta $2007
 
     dec TurnPPUOn
 
+    lda #0
+    sta SkipNMI
+
 GameOverWait:
+
+    ; "press start" fade thing
+    dec TmpY
+    bne @noColor
+
+    lda #8
+    sta TmpY
+
+    ldx TmpX
+    lda DedStartPal, x
+    sta DED_START_PAL   ; Palette 01, first non-BG color
+    inx
+    cpx #6
+    bne @noWrap
+    ldx #0
+@noWrap:
+    stx TmpX
+
+@noColor:
     lda #BUTTON_START
     jsr ButtonPressedP1
     bne @gotoTitle
-    jmp WaitFrame
+    jmp dedSpriteZero
 
 @gotoTitle:
     lda #GS_TITLE
     sta current_gamestate
     inc gamestate_changed
+    ;jmp dedSpriteZero
+
+dedSpriteZero:
+    bit $2002
+    bvs dedSpriteZero
+
+@loop:
+    bit $2002
+    bvc @loop
+
+    lda #4
+    sta $2005
+    lda #PPU_CTRL_HORIZ
+    sta $2000
     jmp WaitFrame
 
 DedText:
     .byte "Game Over!", $00
+
+DedStart:
+    .byte "Press Start", $00
+
+DedStartPal:
+    .byte $2D,$00,$10,$20,$10,$00
 
 DedFade:
     .byte $0F,$17,$2B,$39
