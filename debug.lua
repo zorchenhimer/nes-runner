130a73eb0be68e84c84ec2604513e1a1bfcf65cd
Zorchenhimer <zorchenhimer@gmail.com>
2018-12-11 18:53:07 -0500

    Use tonumber() in the debug script
    Use tonumber() in the lua debug script to get the correct address
    integer from a hex value to avoid converting by hand.

--- debug.lua
+++ debug.lua
@@ -4,11 +4,11 @@ function printInfo()
   state = emu.getState()
   
   emu.drawString(12, 12, "calc_scroll: " .. emu.read(37, 256), 0xFFFFFF, 0xFF00FF00, 1)
-  emu.drawString(12, 21, string.format("meta_column_offset: $%X", emu.read(40, 256), emu.read(1009, 256)), 0xFFFFFF, 0xFF000000, 1)
-  emu.drawString(12, 30, string.format("last_meta_offset: $%X", emu.read(41, 256)))
-  emu.drawString(12, 39, string.format("meta_last_buffer: $%X", emu.read(36, 256)))
-  emu.drawString(12, 48, string.format("meta_last_gen: $%X", emu.read(37, 256)))
-  emu.drawString(12, 57, string.format("gen_countdown: $%X", emu.read(35, 256)))
+  --emu.drawString(12, 21, string.format("meta_column_offset: $%X", emu.read(tonumber("29", 16), 256), emu.read(1009, 256)), 0xFFFFFF, 0xFF000000, 1)
+  emu.drawString(12, 30, string.format("last_meta_offset: $%X", emu.read(tonumber("2A", 16), 256)))
+  emu.drawString(12, 39, string.format("meta_last_buffer: $%X", emu.read(tonumber("23", 16), 256)))
+  emu.drawString(12, 48, string.format("meta_last_gen: $%X", emu.read(tonumber("24", 16), 256)))
+  --emu.drawString(12, 57, string.format("gen_countdown: $%X", emu.read(35, 256)))
   
 end
 
